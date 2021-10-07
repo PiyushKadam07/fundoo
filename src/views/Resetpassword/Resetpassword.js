918a9e9bb9b9d4d1a04f94a6e771768ed8116639
Piyush Kadam <kadam.piyush.17it1041@gmail.com>
2021-10-07 17:01:50 +0530

    [Piyush] added resetpassword page

--- src/views/Resetpassword/Resetpassword.js
+++ src/views/Resetpassword/Resetpassword.js
@@ -1,9 +1,13 @@
 import "./Resetpassword.scss";
+import Logocontent from '@/components/Logocontent/Logocontent.vue';
 import Textbox from '@/components/Textbox/Textbox.vue';
+import Footer from '@/components/Footer/Footer.vue';
 
 export default {
     name: 'Resetpassword',
     components: {
+        Logocontent,
         Textbox,
+        Footer,
     }
 }
--- src/views/Resetpassword/Resetpassword.vue
+++ src/views/Resetpassword/Resetpassword.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="resetpassword">
+        <div class="resetpassword-container">
+            <Logocontent text="Reset Password" />
+
+            <Textbox name="password" label="Password" type="password" />
+
+            <Textbox name="confirm" label="Confirm" type="password" />
+
+            <Footer text="" />
+        </div>
     </div>
 </template>
     
